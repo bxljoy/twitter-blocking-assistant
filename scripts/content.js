c7182b638b40b9ca5c580511da3cab9d003844c0
bxljoy <259500313@qq.com>
2023-12-03 00:32:04 +0100

    fixing the SCP of twitter

--- scripts/content.js
+++ scripts/content.js
@@ -1,57 +1,119 @@
 
 // delay a few sceonds to excute next line
 async function delay(seconds) {
-    console.log('start timer');
     await new Promise(resolve => setTimeout(resolve, seconds * 100));
-    console.log('after 1 second');
 }
 
-// click user actions button
-const userActions = document.querySelector('[data-testid=userActions]');
-userActions.click();
-
-await delay(8);
-
-// click block button
-const blockButton = document.querySelector('[data-testid=block]');
-blockButton.click();
-
-await delay(7);
-
-// click confirm button
-const confirmButton = document.querySelector('[data-testid=confirmationSheetConfirm]');
-confirmButton.click();
-
-await delay(6);
-
-// click back button, back to the twitter accounts list
-const backButton = document.querySelector('[data-testid=app-bar-back]');
-backButton.click();
-
-await delay(5);
+// scroll down the page
+async function scrollDown(num) {
+    await window.scrollBy(0, num * 90);
+}
 
 // attain the list of twitter accounts
 const blockElements = document.querySelectorAll('[data-testid=UserCell]');
-alert(blockElements.length);
+let length = blockElements.length;
+console.log(length);
+// const ch = blockElements[0].clientHeight
+
+// await scrollDown(17);
+
+for (let i = 0; i < length; i++) {
+
+    const newBlockElements = document.querySelectorAll('[data-testid=UserCell]');
+    newBlockElements[i].click();
+    await delay(16);
+
+    // find the send DM button
+    // const sendDMButton = document.querySelector('[data-testid=sendDMFromProfile]');
+
+    // find the unblock button
+    const unblockButton = document.querySelector("div[data-testid$='-unblock']");
+
+    if (!unblockButton) {
+
+        // click user actions button
+        const userActions = document.querySelector('[data-testid=userActions]');
+        userActions.click();
+
+        await delay(18);
+
+        // click block button
+        const blockButton = document.querySelector('[data-testid=block]');
+        blockButton.click();
+
+        await delay(17);
+        // click confirm button
+        const confirmButton = document.querySelector('[data-testid=confirmationSheetConfirm]');
+        confirmButton.click();
+    }
+
+    await delay(16);
+
+    // click back button, back to the twitter accounts list
+    const backButton = document.querySelector('[data-testid=app-bar-back]');
+    backButton.click();
+
+    await delay(15);
+
+}
+await scrollDown(length+17);
+
+
+
 
 
 // javascript:(async () => {
-//     async function delay(seconds) {
-//         console.log('start timer');
-//         await new Promise(resolve => setTimeout(resolve, seconds * 100));
-//         console.log('after 1 second');
+// // delay a few sceonds to excute next line
+// async function delay(seconds) {
+//     await new Promise(resolve => setTimeout(resolve, seconds * 100));
+// }
+
+// async function scrollDown(num) {
+//     window.scrollBy(0, num * 90);
+// }
+
+// // attain the list of twitter accounts
+// const blockElements = document.querySelectorAll('[data-testid=UserCell]');
+// let length = blockElements.length;
+// console.log(length);
+
+// for (let i = 0; i < length; i++) {
+
+//     const newBlockElements = document.querySelectorAll('[data-testid=UserCell]');
+//     newBlockElements[i].click();
+//     await delay(16);
+
+//     // find the send DM button
+//     // const sendDMButton = document.querySelector('[data-testid=sendDMFromProfile]');
+
+//     // find the unblock button
+//     const unblockButton = document.querySelector("div[data-testid$='-unblock']");
+
+//     if (!unblockButton) {
+
+//         // click user actions button
+//         const userActions = document.querySelector('[data-testid=userActions]');
+//         userActions.click();
+
+//         await delay(18);
+
+//         // click block button
+//         const blockButton = document.querySelector('[data-testid=block]');
+//         blockButton.click();
+
+//         await delay(17);
+//         // click confirm button
+//         const confirmButton = document.querySelector('[data-testid=confirmationSheetConfirm]');
+//         confirmButton.click();
 //     }
-    
-//     const userActions = document.querySelector('[data-testid=userActions]');
-//     userActions.click();
-    
-//     await delay(8);
-    
-//     const blockButton = document.querySelector('[data-testid=block]');
-//     blockButton.click();
-    
-//     await delay(7);
-    
-//     const confirmButton = document.querySelector('[data-testid=confirmationSheetConfirm]');
-//     confirmButton.click();
+
+//     await delay(16);
+
+//     // click back button, back to the twitter accounts list
+//     const backButton = document.querySelector('[data-testid=app-bar-back]');
+//     backButton.click();
+
+//     await delay(15);
+// }
+// await scrollDown(length+17);
 // })();
