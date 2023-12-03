e392547627fef7f0b674579abea6387598e16dc1
bxljoy <259500313@qq.com>
2023-12-03 08:56:20 +0100

    updating excute time interval

--- scripts/content.js
+++ scripts/content.js
@@ -62,58 +62,58 @@ await scrollDown(length+17);
 
 
 
-// javascript:(async () => {
-// // delay a few sceonds to excute next line
-// async function delay(seconds) {
-//     await new Promise(resolve => setTimeout(resolve, seconds * 100));
-// }
+javascript:(async () => {
+// delay a few sceonds to excute next line
+async function delay(seconds) {
+    await new Promise(resolve => setTimeout(resolve, seconds * 100));
+}
 
-// async function scrollDown(num) {
-//     window.scrollBy(0, num * 90);
-// }
+async function scrollDown(num) {
+    window.scrollBy(0, num * 70);
+}
 
-// // attain the list of twitter accounts
-// const blockElements = document.querySelectorAll('[data-testid=UserCell]');
-// let length = blockElements.length;
-// console.log(length);
+// attain the list of twitter accounts
+const blockElements = document.querySelectorAll('[data-testid=UserCell]');
+let length = blockElements.length;
+console.log(length);
 
-// for (let i = 0; i < length; i++) {
+for (let i = 0; i < length; i++) {
 
-//     const newBlockElements = document.querySelectorAll('[data-testid=UserCell]');
-//     newBlockElements[i].click();
-//     await delay(16);
+    const newBlockElements = document.querySelectorAll('[data-testid=UserCell]');
+    newBlockElements[i].click();
+    await delay(8);
 
-//     // find the send DM button
-//     // const sendDMButton = document.querySelector('[data-testid=sendDMFromProfile]');
+    // find the send DM button
+    // const sendDMButton = document.querySelector('[data-testid=sendDMFromProfile]');
 
-//     // find the unblock button
-//     const unblockButton = document.querySelector("div[data-testid$='-unblock']");
+    // find the unblock button
+    const unblockButton = document.querySelector("div[data-testid$='-unblock']");
 
-//     if (!unblockButton) {
+    if (!unblockButton) {
 
-//         // click user actions button
-//         const userActions = document.querySelector('[data-testid=userActions]');
-//         userActions.click();
+        // click user actions button
+        const userActions = document.querySelector('[data-testid=userActions]');
+        userActions.click();
 
-//         await delay(18);
+        await delay(9);
 
-//         // click block button
-//         const blockButton = document.querySelector('[data-testid=block]');
-//         blockButton.click();
+        // click block button
+        const blockButton = document.querySelector('[data-testid=block]');
+        blockButton.click();
 
-//         await delay(17);
-//         // click confirm button
-//         const confirmButton = document.querySelector('[data-testid=confirmationSheetConfirm]');
-//         confirmButton.click();
-//     }
+        await delay(8);
+        // click confirm button
+        const confirmButton = document.querySelector('[data-testid=confirmationSheetConfirm]');
+        confirmButton.click();
+    }
 
-//     await delay(16);
+    await delay(9);
 
-//     // click back button, back to the twitter accounts list
-//     const backButton = document.querySelector('[data-testid=app-bar-back]');
-//     backButton.click();
+    // click back button, back to the twitter accounts list
+    const backButton = document.querySelector('[data-testid=app-bar-back]');
+    backButton.click();
 
-//     await delay(15);
-// }
-// await scrollDown(length+17);
-// })();
+    await delay(8);
+}
+await scrollDown(length+17);
+})();
